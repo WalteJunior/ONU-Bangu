b086f5c78fa9741d1be381df025c34f4b6b9001e
Walter Junior <93939362+WalteJunior@users.noreply.github.com>
2022-07-22 19:33:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from "react";
-import { createGlobalStyle } from "styled-components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Security from "./Pages/Security";
 import Nature from "./Pages/Nature";
 import Transport from "./Pages/Transport";
 
-const GlobalStyle = createGlobalStyle`
-body{
-  background-color:white;
-}
-`
 export default class App extends Component {
   render() {
     return (
