0e99766499c39e62ec0a208d900e0a69adf7f305
Walter Junior <93939362+WalteJunior@users.noreply.github.com>
2022-07-22 19:37:16 -0300

    Desafio Final

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ export default class App extends Component {
   render() {
     return (
       <Router>
-        <GlobalStyle />
         <Routes>
           <Route path = '/' element={<Home/>}/>
           <Route path ='/Security' element={<Security />}/>
